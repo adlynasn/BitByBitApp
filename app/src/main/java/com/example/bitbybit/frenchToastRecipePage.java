17a6851e1a5f0a20e754f228fb7168211d9c7800
iqbalzulsafari <u2001296@siswa.um.edu.my>
2023-01-06 18:19:01 +0800

    ui fragments

--- app/src/main/java/com/example/bitbybit/frenchToastRecipePage.java
+++ app/src/main/java/com/example/bitbybit/frenchToastRecipePage.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link recipeSteps#newInstance} factory method to
+ * Use the {@link frenchToastRecipePage#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class recipeSteps extends Fragment {
+public class frenchToastRecipePage extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class recipeSteps extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public recipeSteps() {
+    public frenchToastRecipePage() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class recipeSteps extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment recipeSteps.
+     * @return A new instance of fragment recipeDetailsPage.
      */
     // TODO: Rename and change types and number of parameters
-    public static recipeSteps newInstance(String param1, String param2) {
-        recipeSteps fragment = new recipeSteps();
+    public static frenchToastRecipePage newInstance(String param1, String param2) {
+        frenchToastRecipePage fragment = new frenchToastRecipePage();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,11 +59,17 @@ public class recipeSteps extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_steps, container, false);
+        return inflater.inflate(R.layout.fragment_frech_toast_recipe_details_page, container, false);
     }
 }
 
 /*
 TODO
-- Get steps from database
+- display info from database
+    - Pic
+    - Ingredients
+    - Nutritional value
+- Link to
+    - recipe steps
+    - Previous page
  */
--- app/src/main/java/com/example/bitbybit/tunaPastaRecipePage.java
+++ app/src/main/java/com/example/bitbybit/tunaPastaRecipePage.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link recipeDetailsPage#newInstance} factory method to
+ * Use the {@link tunaPastaRecipePage#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class recipeDetailsPage extends Fragment {
+public class tunaPastaRecipePage extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class recipeDetailsPage extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public recipeDetailsPage() {
+    public tunaPastaRecipePage() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class recipeDetailsPage extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment recipeDetailsPage.
+     * @return A new instance of fragment recipeSteps.
      */
     // TODO: Rename and change types and number of parameters
-    public static recipeDetailsPage newInstance(String param1, String param2) {
-        recipeDetailsPage fragment = new recipeDetailsPage();
+    public static tunaPastaRecipePage newInstance(String param1, String param2) {
+        tunaPastaRecipePage fragment = new tunaPastaRecipePage();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,17 +59,11 @@ public class recipeDetailsPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frech_toast_recipe_details_page, container, false);
+        return inflater.inflate(R.layout.fragment_tuna_pasta_recipe_details_page, container, false);
     }
 }
 
 /*
 TODO
-- display info from database
-    - Pic
-    - Ingredients
-    - Nutritional value
-- Link to
-    - recipe steps
-    - Previous page
+- Get steps from database
  */
