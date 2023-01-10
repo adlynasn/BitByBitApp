2c2b6f3bec7ed262a59ce65d6bea775e378a469b
LAPTOP-IPQAIF8N\abrar <abrar22052002@gmail.com>
2023-01-10 13:52:17 +0800

    abrario1

--- app/src/main/java/com/example/bitbybit/adminViewReportsPage.java
+++ app/src/main/java/com/example/bitbybit/adminViewReportsPage.java
@@ -2,11 +2,15 @@ package com.example.bitbybit;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +64,14 @@ public class adminViewReportsPage extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_view_reports_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button BtnBack = view.findViewById(R.id.returnToAdminHomePageButton);
+        View.OnClickListener OCLBtnBack = v -> Navigation.findNavController(view).navigate(R.id.adminHomePage);
+        BtnBack.setOnClickListener(OCLBtnBack);
     }
 }
