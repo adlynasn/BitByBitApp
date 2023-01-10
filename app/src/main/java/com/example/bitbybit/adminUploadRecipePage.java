4db178d46b77612ff0faf974e3c77b3818ca419f
LAPTOP-IPQAIF8N\abrar <abrar22052002@gmail.com>
2023-01-11 05:32:59 +0800

    abrario1

--- app/src/main/java/com/example/bitbybit/adminUploadRecipePage.java
+++ app/src/main/java/com/example/bitbybit/adminUploadRecipePage.java
@@ -11,7 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -72,15 +79,72 @@ public class adminUploadRecipePage extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Bundle bundle = getArguments();
-        String name=bundle.getString("username");
+        String name = bundle.getString("username");
         bundle.putString("username", name);
 
         Button BtnAddRecipe = view.findViewById(R.id.addRecipeButton);
         View.OnClickListener OCLAddRecipe = v -> {
-            Toast.makeText(getContext(),"The Recipe has been uploaded",Toast.LENGTH_SHORT).show();
-            Navigation.findNavController(view).navigate(R.id.adminHomePage, bundle);
+
+            EditText recipeName = view.findViewById(R.id.recipeName);
+            EditText calories = view.findViewById(R.id.calories);
+            EditText carbohydrates = view.findViewById(R.id.recipeCarbohydrate);
+            EditText fat = view.findViewById(R.id.recipeFat);
+            EditText protein = view.findViewById(R.id.recipeProtein);
+            EditText Ingredients = view.findViewById(R.id.recipeIngredient);
+            EditText Steps = view.findViewById(R.id.recipeSteps);
+
+            AtomicReference<Boolean> status = new AtomicReference<>(false);
+            AtomicReference<Boolean> status1 = new AtomicReference<>(false);
+
+            Thread dataThread = new Thread(() -> {
+
+                try {
+                    Connection connection = Line.getConnection();
+                    System.out.println(recipeName.getText().toString());
+                    PreparedStatement ps = connection.prepareStatement("SELECT * FROM recipe WHERE recipe_id = '" + recipeName.getText().toString() + "'");
+                    PreparedStatement ps1 = connection.prepareStatement("INSERT INTO recipe(recipe_id, recipe_ingredient, recipe_instruction, recipe_calorie, recipe_carbohydrate, recipe_protein, recipe_fat) VALUES('" + recipeName.getText().toString() + "','" + Ingredients.getText().toString() + "','" + Steps.getText().toString() + "','" + calories.getText().toString() + "','" + carbohydrates.getText().toString() + "','" + protein.getText().toString() + "','" + fat.getText().toString() + "')");
+                    ResultSet res = ps.executeQuery();
+
+                    if (res.next()) {
+                        status.set(true);
+
+                    }
+                    if (!status.get()) {
+                        if (recipeName.getText().toString().equals("") || Ingredients.getText().toString().equals("") || Steps.getText().toString().equals("") || calories.getText().toString().equals("") || fat.getText().toString().equals("") || carbohydrates.getText().toString().equals("") || protein.getText().toString().equals("")) {
+                            status1.set(true);
+
+                        } else {
+                            ps1.executeUpdate();
+                        }
+                    }
+
+                    res.close();
+                    ps.close();
+                    ps1.close();
+                    connection.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+            dataThread.start();
+            while (dataThread.isAlive()) {
+
+            }
+            if (status.get()) {
+                Toast.makeText(getContext(), "The recipe or recipe name has already been used or uploaded ", Toast.LENGTH_SHORT).show();
+
+            } else if (status1.get()) {
+                Toast.makeText(getContext(), "Please fill all the section", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(getContext(), "The recipe has been uploaded", Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(view).navigate(R.id.adminHomePage, bundle);
+
+            }
         };
         BtnAddRecipe.setOnClickListener(OCLAddRecipe);
+
 
         Button btnBackToAdminHP = view.findViewById(R.id.returnToAdminHomePageButton);
         View.OnClickListener OCLBackAdminHP = v -> Navigation.findNavController(view).navigate(R.id.adminHomePage, bundle);
