a363542e99269770ff1a81438baecda1cf976c0a
LAPTOP-IPQAIF8N\abrar <abrar22052002@gmail.com>
2023-01-11 19:43:40 +0800

    abrario1

---
+++ app/src/main/java/com/example/bitbybit/NewsStep.java
@@ -0,0 +1,10 @@
+package com.example.bitbybit;
+
+public class NewsStep {
+
+    String step;
+
+    public NewsStep(String step){
+        this.step = step;
+    }
+}
--- app/src/main/java/com/example/bitbybit/foodStepsPage.java
+++ app/src/main/java/com/example/bitbybit/foodStepsPage.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -16,52 +18,22 @@ import android.widget.Button;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link foodStepsPage#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class foodStepsPage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayList<NewsStep> newsStepArraylist;
+    private RecyclerView recyclerview;
 
-    public foodStepsPage() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment foodStepsPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static foodStepsPage newInstance(String param1, String param2) {
-        foodStepsPage fragment = new foodStepsPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -77,7 +49,21 @@ public class foodStepsPage extends Fragment {
 
         Bundle bundle = getArguments();
         String name=bundle.getString("username");
+        String food = bundle.getString("foodbundle");
+        bundle.putString("foodbundle", food);
         bundle.putString("username", name);
+        System.out.println(food);
+        System.out.println(name);
+
+        dataInitialize(food);
+
+        recyclerview = view.findViewById(R.id.recyclerViewSteps);
+        System.out.println("kat sini");
+        recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerview.setHasFixedSize(true);
+        AdaptorStepsRecipe myAdapter = new AdaptorStepsRecipe(getContext(), newsStepArraylist);
+        recyclerview.setAdapter(myAdapter);
+        myAdapter.notifyDataSetChanged();
 
         Button BtnBackFoodIng = view.findViewById(R.id.foodIngredientsPageButton);
         View.OnClickListener OCLBackIngredient = v -> Navigation.findNavController(view).navigate(R.id.action_foodStepsPage_to_foodIngredientPage, bundle);
@@ -108,4 +94,45 @@ public class foodStepsPage extends Fragment {
 
     }
 
+    private void dataInitialize(String food) {
+
+        newsStepArraylist = new ArrayList<>();
+        System.out.println("dalam ni");
+
+
+        Thread dataThread = new Thread(() -> {
+
+            try {
+                System.out.println("dalam connection");
+                Connection connection = Line.getConnection();
+                PreparedStatement ps = connection.prepareStatement("SELECT recipe_instruction FROM recipe WHERE recipe_id = '" +food+ "'");
+                ResultSet res = ps.executeQuery();
+
+                if(res.next()){
+                    String ingredient = res.getString(1);
+                    System.out.println(ingredient);
+                    List<String> list = new ArrayList<String>(Arrays.asList(ingredient.split(">")));
+
+                    for (int i = 0; i < list.size(); i++) {
+                        NewsStep news = new NewsStep(list.get(i));
+                        newsStepArraylist.add(news);
+                    }
+
+                }
+                res.close();
+                connection.close();
+
+
+
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        });
+        dataThread.start();
+        while (dataThread.isAlive()){
+
+        }
+    }
+
 }
+
